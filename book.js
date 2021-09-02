ba999e8ac288d9f41ccf66ecd7d2e1e8482a916e
ohidurgclan <ohidur.ru.2019@gmail.com>
2021-09-02 23:32:25 +0600

    Final Commit

--- book.js
+++ book.js
@@ -1,72 +1,80 @@
-/*------------------------------------------------
-        Default Variable For HTML Elements.
---------------------------------------------------*/
-const resultFound = document.getElementById('resultFound');
-const createCard = document.getElementById('createCard');
-/*---------------------------------------------------------
-            Book Data load Function Starts
------------------------------------------------------------*/
-const searchBooks = () => {
-    const inputText = document.getElementById('inputValue');
-    const inputValue = inputText.value;
-    if (inputValue === '') {
-        resultFound.innerText = `Please Type Your Search Keywords.`;
-        createCard.textContent = '';
-    } else {
-        document.getElementById('createCard').textContent = '';
-        document.getElementById('resultFound').innerText = '';
-        inputText.value = '';
-        const url = `https://openlibrary.org/search.json?q=${inputValue}`;
-        fetch(url)
-            .then(res => res.json())
-            .then(data => displayResult(data));
+// function for data fetch from api
+const fetchData = async (searchValue) => {
+    document.getElementById("resultFound").textContent = "";
+    spinnerShow("block");
+    containerShow("none");
+    try {
+        const result = await fetch(
+            `https://openlibrary.org/search.json?q=${searchValue}`
+        );
+        const data = await result.json();
+
+        showData(data);
+    } catch (error) {
+        spinnerShow("none");
+        document.getElementById("createCard").innerHTML = `
+    <h3 class='text-light'>No Data Found</h3>
+    `;
     }
-}
-/*---------------------------------------------------------
-            Book Data load Function Ends.
------------------------------------------------------------*/
-/*---------------------------------------------------------
-            Display Result Function Starts
------------------------------------------------------------*/
-const displayResult = getData => {
-    resultFound.innerText = `${getData.numFound} Result Founds;`
-    createCard.textContent = '';
-    if (getData.numFound === 0) {
-        resultFound.innerText = `Nothing Found! in Your Search Keyword.`;
-    } else {
-        const displayData = getData.docs;
-        // for each loop start
-        displayData.forEach(data => {
-            const imgurL = () => {
-                if (data.cover_i) {
-                    return `https://covers.openlibrary.org/b/id/${data.cover_i}-M.jpg`;
-                } else {
-                    return "http://dirtydeedsindustries.com/wp-content/uploads/2016/08/placeholder.jpg.png";
-                }
-            };
-            const thumbImg = imgurL();
-                    // Creating card div
-            const div = document.createElement('div');
-            div.classList.add('col-xxl-4','col-xl-4','col-md-4','mt-5','card-deck');
-            div.innerHTML = `
-            <div class="card card-shadow h-100 p-3 m-3">
-                <img style="height: 20rem;" class="img img-fluid" src=${thumbImg}>
-                <h4 class="mt-3"><span class="fw-bold">Name:</span> ${data.title.slice(0, 15)}</h4>
-                <p class="p-0 mt-1"><span class="fw-bold">Author:</span> ${data.author_name[0]}</p>
-                <p class="p-0 mt-1"><span class="fw-bold">Publisher:</span> ${data.publisher.slice(0, 5)}</p>
-                <p class="p-0 mt-1"><span class="fw-bold">First Publish Year:</span> ${data.first_publish_year}</p>
-            </div>
-            `               // Ending card div end
-            createCard.appendChild(div);
-        });                 // Forach loop end
+};
+
+// function for receving search key word by clicking search button
+document.getElementById('button-addon2').addEventListener("click", () => {
+    const searchValue = document.getElementById("inputValue").value;
+    fetchData(searchValue);
+
+    document.getElementById("inputValue").value = "";
+});
+
+// function for showing api data in ui
+const showData = (bookData) => {
+    document.getElementById(
+        "resultFound"
+    ).innerText = `Total Book: ${bookData.numFound}`;
+
+    const container = document.getElementById("createCard");
+    container.textContent = "";
+
+    if (bookData.docs.length === 0) {
+        container.innerHTML = `
+      <h5 class="text-primary">Nothing Found!</h5>
+      `;
     }
-}
-/*---------------------------------------------------------
-            Display Result Function Starts
------------------------------------------------------------*/
-/*- - - - - Programming Hero Dynamic API Assignment - - - - - -*/
-/*-------------------------------------------------------
-      Assignmet-6 Submited By Md Ohidur Rahman.
-         Thank You Programming Hero Examiner.
-                    Have a Nice Day!
---------------------------------------------------------*/
+    bookData.docs.slice(0, 15).forEach((book) => {
+        const img = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        const alternateImg =
+            'http://dirtydeedsindustries.com/wp-content/uploads/2016/08/placeholder.jpg.png';
+        const div = document.createElement("div");
+        div.innerHTML = `
+ <div class="card card-shadow h-100 p-3 m-3"> ${book.cover_i ? ` <img class="img img-fluid h-75" src=${img}>` : `<img class="img img-fluid h-75" src=${alternateImg}>`
+            } 
+    <h5 class="mt-3"><span class="fw-bold">Name:</span> ${book.title}</h5>
+    <p class="mt-3"><span class="fw-bold">Author Name:</span>  ${book.author_name?.[0]} </p>
+    <p class="p-0 mt-1"><span class="fw-bold">Publisher:</span> ${book.publisher?.[0]} </p>
+    <pclass="p-0 mt-1"><span class="fw-bold">First Publish Year:</span> ${book.first_publish_year}</p>
+    </div>
+    `;
+        div.classList.add('col-xxl-12', 'col-xl-12', 'col-md-12', 'mt-5', 'card-deck');
+        container.appendChild(div);
+    });
+    containerShow("block");
+    spinnerShow("none");
+};
+
+// show spinner function
+const spinnerShow = (display) => {
+    if (display === "none") {
+        document.getElementById("spinner").style.display = "none";
+    } else {
+        document.getElementById("spinner").style.display = "block";
+    }
+};
+
+// books container show or not function
+const containerShow = (display) => {
+    if (display === "none") {
+        document.getElementById("createCard").style.display = "none";
+    } else {
+        document.getElementById("createCard").style.display = "grid";
+    }
+};
